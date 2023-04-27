76a6e4d38f36aa919fdee1f4d3b6816dcf0f512c
michaelmarkus4690 <116219341+michaelmarkus4690@users.noreply.github.com>
2023-04-26 21:14:33 -0700

    mountainglow

--- java.js
+++ java.js
@@ -1,8 +1,16 @@
 var popup = document.getElementById("popup-tab");
 var popupHeader = popup.querySelector(".popup-header");
 var popupClose = popup.querySelector(".popup-close");
+const verticalCenter = window.scrollY + (window.innerHeight / 2);
+const horizontalCenter = window.innerWidth / 2;
 
-// Make the popup draggable
+window.addEventListener("scroll", () => {
+  const verticalCenter = window.scrollY + (window.innerHeight / 2);
+  const horizontalCenter = window.innerWidth / 2;
+  return verticalCenter, horizontalCenter
+});
+
+
 popupHeader.addEventListener("mousedown", dragStart);
 document.addEventListener("mousemove", drag);
 document.addEventListener("mouseup", dragEnd);
@@ -29,43 +37,12 @@ popupClose.addEventListener("click", function() {
   popup.style.display = "none";
 });
 
-function centerPopup() {
-  // Calculate the position of the center of the viewport
-  var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-  var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-  var centerX = viewportWidth / 2;
-  var centerY = viewportHeight / 2;
-
-  // Calculate the position of the top-left corner of the popup
-  var popupWidth = popup.offsetWidth;
-  var popupHeight = popup.offsetHeight;
-  var popupX = centerX - (popupWidth / 2);
-  var popupY = centerY - (popupHeight / 2);
-
-  // Set the position of the popup
-  popup.style.left = popupX + "px";
-  popup.style.top = popupY + "px";
-  popup.style.display = "block";
-}
-
-// Call centerPopup() when the page loads
-window.addEventListener("load", centerPopup);
+;
 
 function calculateage() {
-  // Calculate the position of the center of the viewport
-  var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-  var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-  var centerX = viewportWidth / 2;
-  var centerY = viewportHeight / 2;
-
-  // Calculate the position of the top-left corner of the popup
-  var popupWidth = popup.offsetWidth;
-  var popupHeight = popup.offsetHeight;
-  var popupX = centerX - (popupWidth / 2);
-  var popupY = centerY - (popupHeight / 2);
-
-  // Set the position of the popup
-  popup.style.left = popupX + "px";
-  popup.style.top = popupY + "px";
+  const verticalCenter = window.scrollY + (window.innerHeight / 2);
+  const horizontalCenter = window.innerWidth / 2;
+  popup.style.top = `${verticalCenter - (popup.offsetHeight / 2)}px`;
+  popup.style.left = `${horizontalCenter - (popup.offsetWidth / 2)}px`;
   popup.style.display = "block";
 }
