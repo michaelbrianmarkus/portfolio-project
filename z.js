1ffb7f365ec745a378531faedf7554106bf439db
michaelmarkus4690 <116219341+michaelmarkus4690@users.noreply.github.com>
2023-05-07 21:21:37 -0700

    sunday

--- z.js
+++ z.js
@@ -13,3 +13,39 @@ function changeScreen() {
   var tvScreen = document.getElementById("tvscreen");
   tvScreen.style.backgroundImage = "url('images/cart 1.png')";
 }
+
+const slideshowImages = document.querySelector(".slideshow-images");
+const navigationDots = document.querySelectorAll(".dot");
+
+let currentSlide = 0;
+
+setInterval(() => {
+  currentSlide++;
+  if (currentSlide >= navigationDots.length) {
+    currentSlide = 0;
+  }
+  changeSlide(currentSlide);
+}, 5000);
+changeSlide(currentSlide);
+
+
+navigationDots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    changeSlide(index);
+  });
+});
+
+function changeSlide(index) {
+  currentSlide = index;
+
+  navigationDots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+  navigationDots[currentSlide].classList.add("active");
+
+  const images = slideshowImages.querySelectorAll("img");
+  images.forEach((image) => {
+    image.classList.remove("active");
+  });
+  images[currentSlide].classList.add("active");
+}
