73d8105e9a72c94580e8df6d840b156e3c603ed5
michaelmarkus4690 <116219341+michaelmarkus4690@users.noreply.github.com>
2023-05-08 22:02:01 -0700

    more changes

--- java.js
+++ java.js
@@ -1,26 +1,3 @@
-var popup = document.getElementById("popup-tab");
-var popupHeader = popup.querySelector(".popup-header");
-var popupClose = popup.querySelector(".popup-close");
-const verticalCenter = window.scrollY + (window.innerHeight / 2);
-const horizontalCenter = window.innerWidth / 2;
-
-window.addEventListener("scroll", () => {
-  const verticalCenter = window.scrollY + (window.innerHeight / 2);
-  const horizontalCenter = window.innerWidth / 2;
-  return verticalCenter, horizontalCenter
-});
-
-
-popupHeader.addEventListener("mousedown", dragStart);
-document.addEventListener("mousemove", drag);
-document.addEventListener("mouseup", dragEnd);
-
-function dragStart(e) {
-  popup.classList.add("draggable");
-  dragX = e.clientX - popup.offsetLeft;
-  dragY = e.clientY - popup.offsetTop;
-}
-
 function drag(e) {
   if (popup.classList.contains("draggable")) {
     popup.style.left = (e.clientX - dragX) + "px";
@@ -31,18 +8,97 @@ function drag(e) {
 function dragEnd(e) {
   popup.classList.remove("draggable");
 }
+function changeScreen() {
+  var tvScreen = document.getElementById("tvscreen");
+  tvScreen.style.backgroundImage = "url('images/cart 1.png')";
+}
 
-// Close the popup when the close button is clicked
-popupClose.addEventListener("click", function() {
-  popup.style.display = "none";
+const slideshowImages = document.querySelector(".slideshow-images");
+const navigationDots = document.querySelectorAll(".dot");
+
+let currentSlide = 0;
+
+setInterval(() => {
+  currentSlide++;
+  if (currentSlide >= navigationDots.length) {
+    currentSlide = 0;
+  }
+  changeSlide(currentSlide);
+}, 5000);
+changeSlide(currentSlide);
+
+
+navigationDots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    changeSlide(index);
+  });
 });
 
-;
+function changeSlide(index) {
+  currentSlide = index;
 
-function calculateage() {
-  const verticalCenter = window.scrollY + (window.innerHeight / 2);
-  const horizontalCenter = window.innerWidth / 2;
-  popup.style.top = `${verticalCenter - (popup.offsetHeight / 2)}px`;
-  popup.style.left = `${horizontalCenter - (popup.offsetWidth / 2)}px`;
-  popup.style.display = "block";
+  navigationDots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+  navigationDots[currentSlide].classList.add("active");
+
+  const images = slideshowImages.querySelectorAll("img");
+  images.forEach((image) => {
+    image.classList.remove("active");
+  });
+  images[currentSlide].classList.add("active");
 }
+
+var counter = 1;
+
+function incrementCounter() {
+  counter === (counter  + 1)
+  console.log("Counter is now " + counter);
+}
+
+function leftbutton(){
+  var counter = 1;
+  var audio = new Audio('audio/click.mp3');
+  audio.play();
+  console.log('Left button has been clicked')
+  incrementCounter()
+  }
+
+
+function rightbutton(){
+  var audio = new Audio('audio/click.mp3');
+  audio.play();
+  console.log('Right button has been clicked')
+}
+
+function test(){
+  document.getElementById("tvscreen").style.backgroundImage = "url(images/cart 1.png)";
+  console.log("test")
+}
+
+
+window.addEventListener('load', function() {
+  document.body.classList.add('loaded');
+});
+
+const animationTrigger = 500;
+
+// Get a reference to the element you want to animate
+const myElement = document.getElementById('myElement');
+
+// Set a flag to keep track of whether the animation has played
+let animationPlayed = false;
+
+// Listen for scroll events on the window object
+window.addEventListener('scroll', function() {
+
+  // Check if the current scroll position is greater than the animation trigger position
+  if (window.scrollY > animationTrigger && !animationPlayed) {
+
+    // Play the animation
+    myElement.classList.add('animate');
+
+    // Set the flag to true so the animation only plays once
+    animationPlayed = true;
+  }
+});
