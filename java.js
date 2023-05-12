41dee46c848ad63948513aed8b6a8e16b8e9d948
michaelmarkus4690 <116219341+michaelmarkus4690@users.noreply.github.com>
2023-05-12 13:50:41 -0700

    channhes

--- java.js
+++ java.js
@@ -2,18 +2,22 @@ let imagenumber = 0;
 
 function updatetv(){
 if (imagenumber === 0){
-  document.getElementById("tvscreen").style.backgroundImage = "url(images/cart 1.png)"
+  document.getElementById("tvscreen").style.backgroundImage = "url(images/nipd%201.png)";
 }
 if (imagenumber === 1){
   console.log('1')
-  document.getElementById("tvscreen").style.backgroundImage = "images/phishing 1.png"
+  document.getElementById("tvscreen").style.backgroundImage = "url(images/phishing%201.png)";
 
 }
 if (imagenumber === 2){
   console.log('2')
+  document.getElementById("tvscreen").style.backgroundImage = "url(images/sorry%20project%201.png)";
+
 }
 if (imagenumber === 3){
   console.log('3')
+  document.getElementById("tvscreen").style.backgroundImage = "url(images/phishing%201.png)";
+
 }
 
 
@@ -35,21 +39,23 @@ function leftbutton(){
   }
 
 
-function rightbutton(){
-  var audio = new Audio('audio/click.mp3');
-  audio.play();
-  console.log('Right button has been clicked')
-  imagenumber -= 1 
-  if (imagenumber > 3){
-    imagenumber = 0
-  }
 
-  else if (imagenumber < 0){
-   imagenumber = 0
-  } 
-  updatetv()
-  
-}
+  function rightbutton(){
+    var audio = new Audio('audio/click.mp3');
+    audio.play();
+    console.log('Right button has been clicked');
+    
+    imagenumber += 1;
+    
+    if (imagenumber > 3){
+      imagenumber = 0;
+    }
+    else if (imagenumber < 0){
+      imagenumber = 0;
+    }
+    
+    updatetv();
+  }
 
 setInterval(function() {
   imagenumber += 1 
